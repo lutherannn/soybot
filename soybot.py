76a2c3e237479e3e1e9aa8edf774cfaad90d1624
lutherannn <michael.eskelin@gmail.com>
2022-05-13 22:33:42 -0400

    Removed youtube stuff, added roll

--- soybot.py
+++ soybot.py
@@ -1,75 +1,37 @@
-import discord, youtube_dl, os
+import discord, os, random
 from discord.ext import commands
 from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix="!")
 
-
+# 4chan style post rolling for roll threads
 @client.command()
-async def play(ctx, url: str):
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send(
-            "Wait for the current playing music to end or use the 'stop' command"
-        )
-        return
+async def roll(ctx):
+    dubs, trips, quads = False, False, False
+    nums = []
+    for _ in range(10):
+        nums.append(str(random.randrange(0, 9)))
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    await ctx.send("".join(nums))
 
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    if nums[-1] == nums[-2]:
+        dubs = True
+    if nums[-1] == nums[-2] and nums[-1] == nums[-3]:
+        trips = True
+    if nums[-1] == nums[-2] and nums[-1] == nums[-3] and nums[-1] == nums[-4]:
+        quads = True
 
+    if dubs and trips:
+        dubs = False
+    if quads and trips:
+        trips = False
 
-@client.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-
-@client.command()
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Currently no audio is playing.")
-
-
-@client.command()
-async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-    else:
-        await ctx.send("The audio is not paused.")
-
-
-@client.command()
-async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
+    if dubs and not trips and not quads:
+        await ctx.send("Dubs!")
+    if trips and not dubs and not quads:
+        await ctx.send("Trips!")
+    if quads and not dubs and not trips:
+        await ctx.send("Quads!")
 
 
 load_dotenv()
