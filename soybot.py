58baba1f3fcc2ce4c87ca0b723df4e94f1d91d23
lutherannn <michael.eskelin@gmaiil.com>
2022-11-21 22:56:03 -0500

    ok that was just annoying

--- soybot.py
+++ soybot.py
@@ -80,19 +80,6 @@ async def on_message(message):
     if message.content.startswith("BILLS"):
         await message.channel.send("MAFIA")
 
-    if "m" in message.content or "M" in message.content and not message.author.bot:
-        newMsg = message.content
-        print(newMsg)
-        while "m" in newMsg or "M" in newMsg:
-            newMsg = newMsg.replace("m", "\*")
-            newMsg = newMsg.replace("M", "\*")
-            print(newMsg)
-        await message.channel.send(
-            "Please refrain fro\* using that letter this week, Go Bucs!"
-        )
-        if "m" not in newMsg and "M" not in newMsg:
-            await message.channel.send("Your \*essage should read: " + newMsg)
-        # print(message.author.bot)
     await client.process_commands(message)
 
 
